937ba9c11839025408f15206e3ff15f12ab21469
小燕子 <xgsardh@163.com>
2024-04-24 04:00:04 +0800

    游客权限的controller层 新增办法: findPublicAllGroup, findPublicOneGroup 修改办法 register retrievePassword login

--- qgFundManagementSystem/src/main/java/com/qg/controller/TouristServlet.java
+++ qgFundManagementSystem/src/main/java/com/qg/controller/TouristServlet.java
@@ -1,8 +1,7 @@
 package com.qg.controller;
 
-import com.qg.factory.DaoFactory;
-import com.qg.service.TouristService;
 import com.qg.service.UserService;
+import com.qg.util.JwtUtils.JwtUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.System.out;
 
 @WebServlet("/LoginAndRegisterServlet")
-public class LoginAndRegisterServlet extends BaseServlet {
+public class TouristServlet extends BaseServlet {
 
     public void login(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
-        String username = request.getParameter("userId");
+        String userId = request.getParameter("userId");
         String password = request.getParameter("password");
-        Integer loginStatus = TouristService.getInstance().login(username, password);
-        if(loginStatus == 1){
-            response.getWriter().write("{\"status\":200, \"message\":\"登录成功\"}");
+        Integer loginStatus = com.qg.service.TouristService.getInstance().login(userId, password);
+
+        if (loginStatus == 1) {
+            // 生成JWT
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("userId", userId);
+            String token = JwtUtils.generateJwt(claims);
+
+            // 将JWT发送给前端
+            response.getWriter().write("{\"status\":200, \"message\":\"登录成功\", \"token\":\"" + token + "\"}");
         } else {
             response.getWriter().write("{\"status\":402, \"message\":\"登录失败\"}");
         }
@@ -39,7 +49,7 @@ public class LoginAndRegisterServlet extends BaseServlet {
 
         Long registerStatus = null;
         try {
-            registerStatus = TouristService.getInstance().register(userName, nickname, password, email, phone);
+            registerStatus = com.qg.service.TouristService.getInstance().register(userName, nickname, password, email, phone);
         } catch (SQLException e) {
             if(e.getErrorCode() == 1062) {
                 // 信息已被使用
@@ -63,15 +73,32 @@ public class LoginAndRegisterServlet extends BaseServlet {
         String phoneNumber = request.getParameter("phoneNumber");
         String email = request.getParameter("email");
         String passwordHash = request.getParameter("passwordHash");
-        System.out.println(phoneNumber);
-        System.out.println(email);
-        System.out.println(passwordHash);
-        Long registerStatus = TouristService.getInstance().retrievePassword(phoneNumber, email, passwordHash);
-        System.out.println(registerStatus);
+        out.println(phoneNumber);
+        out.println(email);
+        out.println(passwordHash);
+        Long registerStatus = com.qg.service.TouristService.getInstance().retrievePassword(phoneNumber, email, passwordHash);
+        out.println(registerStatus);
         if(registerStatus == 0L) {
             response.getWriter().write("{\"status\":201, \"message\":\"找回密码失败\"}");
         } else {
             response.getWriter().write("{\"status\":200, \"message\":\"找回密码成功\", \"userId\":" + registerStatus + "}");
         }
     }
+
+    public void findPublicAllGroup(HttpServletRequest request, HttpServletResponse response)
+            throws  ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        String viewGroup = UserService.getInstance().viewGroup();
+        // 发送JSON响应
+        response.getWriter().write("{\"status\":200, \"message\":\"找回密码成功\", \"groups\":" + viewGroup + "}");
+    }
+    public void findPublicOneGroup(HttpServletRequest request, HttpServletResponse response)
+            throws  ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        String groupId = request.getParameter("groupId");
+        String viewGroup = UserService.getInstance().SearchGruop(Long.valueOf(groupId));
+        // 发送JSON响应
+        response.getWriter().write("{\"status\":200, \"message\":\"找回密码成功\", \"groups\":" + viewGroup + "}");
+    }
+
 }
