4919c9ed38f420a4b5a18f999a195acefcff7f63
小燕子 <xgsardh@163.com>
2024-04-24 04:00:58 +0800

    游客的service层 新增办法: register, retrievePassword, SearchGruop, viewGroup

--- qgFundManagementSystem/src/main/java/com/qg/service/TouristService.java
+++ qgFundManagementSystem/src/main/java/com/qg/service/TouristService.java
@@ -1,8 +1,15 @@
 package com.qg.service;
 
-import com.qg.dao.UserDao;
-import com.qg.dao.impl.UserImpl;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.qg.factory.DaoFactory;
+import com.qg.po.Group;
 import com.qg.po.User;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class TouristService {
 
@@ -26,8 +33,20 @@ public class TouristService {
     /**
      * 注册
      */
-    public void register() {
-
+    public Long register(String userName, String nickname, String passwordHash, String email, String phoneNumber) throws SQLException{
+        User user = new User(
+                userName,
+                nickname,
+                passwordHash,
+                email,
+                phoneNumber,
+                "0",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+        Long userId = null;
+        userId = DaoFactory.getUserDao().save(user);
+        return userId;
     }
 
     /**
@@ -39,7 +58,7 @@ public class TouristService {
         if (!userId.matches("-?\\d+")) {
             return 0;
         }
-        User user = UserImpl.getInstance().findById(Long.valueOf(userId)).get(0);
+        User user = DaoFactory.getUserDao().findById(Long.valueOf(userId)).get(0);
         if(user == null) {
             return 0;
         } else if (!user.getPasswordHash().equals(password)) {
@@ -50,16 +69,58 @@ public class TouristService {
     }
 
     /**
+     * 找回密码: 依靠手机号来作为主要凭据
+     */
+    public Long retrievePassword(String phoneNumber, String email, String passwordHash) {
+        User user = DaoFactory.getUserDao().findByPhoneNumber(phoneNumber).get(0);
+        if (user.getEmail().equals(email)) {
+            user.setPasswordHash(passwordHash);
+            try {
+                DaoFactory.getUserDao().update(user);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return user.getUserId();
+        }
+        return 0L;
+    }
+
+    /**
      * 搜索公开群组
      */
-    public void SearchGruop() {
-
+    public String SearchGruop(Long groupId) {
+        List<Group> groups= DaoFactory.getGroupDao().findByGroupId(groupId);
+        JSONArray jsonArray = new JSONArray();
+        for (Group group : groups) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("group_id", group.getGroupId());
+            jsonObject.put("group_name", group.getGroupName());
+            jsonObject.put("create_by", group.getCreatedBy());
+            jsonObject.put("description", group.getDescription());
+            jsonArray.add(jsonObject);
+        }
+        String output  = JSON.toJSONString(jsonArray);
+        System.out.println(output);
+        return output;
     }
 
     /**
      * 查看公开群组
      */
-    public void viewGroup() {
-
+    public String viewGroup() {
+        List<Group> groups= DaoFactory.getGroupDao().findPublicAll();
+        JSONArray jsonArray = new JSONArray();
+        for (Group group : groups) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("group_id", group.getGroupId());
+            jsonObject.put("group_name", group.getGroupName());
+            jsonObject.put("create_by", group.getCreatedBy());
+            jsonObject.put("description", group.getDescription());
+            jsonArray.add(jsonObject);
+        }
+        String output  = JSON.toJSONString(jsonArray);
+        System.out.println(output);
+        return output;
     }
+
 }
