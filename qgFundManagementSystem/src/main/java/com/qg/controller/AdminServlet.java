bf2f93be59a69e9aea54fec9dd55adca5b8080e1
小燕子 <xgsardh@163.com>
2024-04-24 03:46:39 +0800

    网站管理员权限的controller层中 实现以下方法: sureCreateGroup refuseCreateGroup 更新了以下方法: viewCreateGroupList

--- qgFundManagementSystem/src/main/java/com/qg/controller/AdminServlet.java
+++ qgFundManagementSystem/src/main/java/com/qg/controller/AdminServlet.java
@@ -24,7 +24,44 @@ public class AdminServlet extends BaseServlet {
             return;
         }
         String createGroupList = AdminService.getInstance().viewCreateGroupList();
-        response.getWriter().write("{\"status\":200, \"message\":\"获得创建群组列表成功\", \"data\":" + createGroupList + "}");
+        if(createGroupList.equals("[]")) {
+            response.getWriter().write("{\"status\":200, \"message\":\"暂无待审核列表\", \"data\":\"" + createGroupList + "\"}");
+        }else {
+            response.getWriter().write("{\"status\":200, \"message\":\"获得创建群组列表成功\", \"data\":" + createGroupList + "}");
+        }
+    }
+
+    public void sureCreateGroup(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+
+        Long userId = HelpServlet.getInstance().getUserIdFromJwt(request, response);
+        if(userId == 0L) {
+            response.getWriter().write("{\"status\":201, \"message\":\"通过审核出错,id错误\", \"data\":" + 0 + "}");
+            return;
+        }
+        String beDoUserId = request.getParameter("user_id");
+        String permissionChangeId = request.getParameter("permission_change_id");
+        String sureCreateGroup = AdminService.getInstance().sureCreateGroup(Long.valueOf(beDoUserId), Long.valueOf(permissionChangeId));
+
+        response.getWriter().write("{\"status\":200, \"message\":\"通过审核成功\", \"data\":\"" + sureCreateGroup + "\"}");
+
+    }
+
+    public void refuseCreateGroup(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+
+        Long userId = HelpServlet.getInstance().getUserIdFromJwt(request, response);
+        if(userId == 0L) {
+            response.getWriter().write("{\"status\":201, \"message\":\"通过审核出错\", \"data\":" + 0 + "}");
+            return;
+        }
+        String beDoUserId = request.getParameter("user_id");
+        String permissionChangeId = request.getParameter("permission_change_id");
+        String sureCreateGroup = AdminService.getInstance().refuseCreateGroup(Long.valueOf(beDoUserId), Long.valueOf(permissionChangeId));
+
+        response.getWriter().write("{\"status\":200, \"message\":\"已成功退回\", \"data\":\"" + sureCreateGroup + "\"}");
 
     }
 
