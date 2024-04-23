470baef683b44e5cd400fab097a299ee4a7328e8
小燕子 <xgsardh@163.com>
2024-04-24 04:05:59 +0800

    新增BaseServlet, 其他Servlet通过继承其, 通过反射找到合适的方法

--- qgFundManagementSystem/src/main/java/com/qg/controller/BaseServlet.java
+++ qgFundManagementSystem/src/main/java/com/qg/controller/BaseServlet.java
@@ -1,12 +1,14 @@
 package com.qg.controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
+@WebServlet("/BaseServlet")
 public class BaseServlet extends HttpServlet {
 
     /**
