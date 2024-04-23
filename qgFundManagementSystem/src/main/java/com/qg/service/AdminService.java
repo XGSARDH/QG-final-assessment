c64f748a98bb379ad82d5e4d8091bcec7a3cc0e9
小燕子 <xgsardh@163.com>
2024-04-24 03:45:36 +0800

    网站管理员权限的service层中实现以下方法: sureCreateGroup

--- qgFundManagementSystem/src/main/java/com/qg/service/AdminService.java
+++ qgFundManagementSystem/src/main/java/com/qg/service/AdminService.java
@@ -4,8 +4,11 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.qg.factory.DaoFactory;
+import com.qg.po.Group;
 import com.qg.po.PermissionChange;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class AdminService {
@@ -38,7 +41,47 @@ public class AdminService {
             jsonArray.add(jsonObject);
         }
         String output  = JSON.toJSONString(jsonArray);
-        System.out.println(output);
         return output;
+    }
+
+    public String sureCreateGroup(Long userId, Long permissionChangeId) {
+        PermissionChange permissionChange = DaoFactory.getPermissionChangeDao().findAuditId(permissionChangeId).get(0);
+        if(permissionChange.getStatus() != 0) {
+            return "已处理";
+        }
+        Group group = new Group();
+        group.setGroupName("默认名");
+        group.setCreatedBy(userId);
+        group.setIsPublic(1);
+        group.setGmtCreate(LocalDateTime.now());
+        group.setGmtModified(LocalDateTime.now());
+        try {
+            permissionChange.setGroupId(DaoFactory.getGroupDao().save(group));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        permissionChange.setGmtModified(LocalDateTime.now());
+        permissionChange.setStatus(1);
+        try {
+            DaoFactory.getPermissionChangeDao().update(permissionChange);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return "成功通过";
+    }
+    public String refuseCreateGroup(Long userId, Long permissionChangeId) {
+        PermissionChange permissionChange = DaoFactory.getPermissionChangeDao().findAuditId(permissionChangeId).get(0);
+        if(permissionChange.getStatus() != 0) {
+            return "已拒绝";
+        }
+        permissionChange.setGmtModified(LocalDateTime.now());
+        permissionChange.setStatus(-1);
+        permissionChange.setGroupId(null);
+        try {
+            DaoFactory.getPermissionChangeDao().update(permissionChange);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return "成功通过";
     }
 }
